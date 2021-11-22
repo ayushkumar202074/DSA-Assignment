b1adb272b3ed819396e84460ad60d53b00a90623
ayushkumar202074 <84980670+ayushkumar202074@users.noreply.github.com>
2021-11-22 21:04:05 +0530

    Add files via upload

---
+++ Program-13.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left, *right;
+
+    Node(int data)
+    {
+        this->data = data;
+        left = right = NULL;
+    }
+};
+
+// Printing a Binary Tree according to PostOrder Traversal
+
+void PostOrder(Node *node)
+{
+    if(node == NULL)
+        return;
+    
+    PostOrder(node->left);
+    PostOrder(node->right);
+    cout<<node->data<<" ";
+}
+
+// Printing a Binary Tree according to InOrder Traversal
+
+void InOrder(Node *node)
+{
+    if(node == NULL)
+        return;
+    
+    InOrder(node->left);
+    cout<<node->data<<" ";
+    InOrder(node->right);
+}
+
+// Printing a Binary Tree according to PreOrder Traversal
+
+void PreOrder(Node *node)
+{
+    if(node == NULL)
+        return;
+
+    cout<<node->data<<" ";
+    PreOrder(node->left);
+    PreOrder(node->right);
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+
+    cout<<"\nPreOrder Traversal of Binary Tree is \n";
+    PreOrder(root);
+
+    cout<<"\nInOrder Traversal of Binary Tree is \n";
+    InOrder(root);
+
+    cout<<"\nPostOrder Traversal of Binary Tree is \n";
+    PostOrder(root);
+
+    cout<<endl;
+
+    return 0;
+}
